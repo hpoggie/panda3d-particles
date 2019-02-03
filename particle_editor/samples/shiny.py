45e2a08f9177c30191e09c8c73e71a7bff44d3d6
hpoggie <henry.poggie@gmail.com>
2019-02-03 17:12:07 -0500

    Fix sample

--- particle_editor/samples/shiny.py
+++ particle_editor/samples/shiny.py
@@ -1,10 +1,10 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import loadPrcFileData
-import vfx_loader
+from particle_editor import vfx_loader
 
 
-loadPrcFileData('', 'model-path assets')
-loadPrcFileData('', 'model-path samples/shiny')
+shinyPath = vfx_loader.loadAssetPath('samples/shiny')
+print("[SAMPLE] Loaded asset path: " + shinyPath)
 
 
 class App(ShowBase):
@@ -16,7 +16,7 @@ class App(ShowBase):
         base.camera.setPos(30.0, -30.0, 10.0)
         base.camera.lookAt((0.0, 0.0, -1.0))
 
-        p = vfx_loader.load('samples/shiny/shiny.json')
+        p = vfx_loader.load(shinyPath + '/shiny.json')
         p.start(parent=self.render, renderParent=render)
 
 App().run()
